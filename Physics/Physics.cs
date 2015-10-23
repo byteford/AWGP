204f491ccb6021a64d1a9886f888ec86b0bb48ea
TreeOfGrace <gracetree3@gmail.com>
2015-10-23 13:56:59 +0100

    namespace physics

--- Physics/Physics.cs
+++ Physics/Physics.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using BlinkByte.Core.Module;
 
-namespace Physics
+namespace BlinkByte.Physics
 {
     public class Physics : BlinkByte.Core.Module.IModule
     {
