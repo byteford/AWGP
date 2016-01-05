8d8d03f5714923ad1752935403d7a18b4908a50d
LewisA <lewis.andrews@blinkbyte.co.uk>
2016-01-05 18:12:32 +0000

    sound code added to sfml and audios folder added to main

--- SFMLSound/Sound.cs
+++ SFMLSound/Sound.cs
@@ -8,5 +8,13 @@ namespace BlinkByte.SFMLSound
 {
     public class Sound : BlinkByte.Sound.ISound
     {
+        public void init(){
+        SFML.Audio.SoundBuffer buffer = new SFML.Audio.SoundBuffer("sound.wav");
+//buffer.loadFromFile("sound.wav");
+
+SFML.Audio.Sound sound = new SFML.Audio.Sound(buffer);
+//sound.setBuffer(buffer);
+sound.Play();   
+        }
     }
 }
