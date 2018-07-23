ff5a436079713a5f7a31daaad123683e73f4d59b
James sandford <sandford.james@gmail.com>
2018-07-23 15:25:35 +0900

    made a squre comp

---
+++ Graphics/Componates/Square2DComp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlinkByte.Graphics.Componates
+{
+    public class Square2DComp: Shape2DComp
+    {
+        public int height, width;
+        public Square2DComp() 
+            : base()
+        {
+            shape = shapeType.Square;
+        }
+        public override void Draw()
+        {
+        }
+    }
+}
---
+++ SFMLGraphics/SFMLSquare2DComp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlinkByte.Utilitys;
+namespace BlinkByte.SFMLGraphics
+{
+    class SFMLSquare2DComp : BlinkByte.Graphics.Componates.Square2DComp
+    {
+        SFML.Graphics.RectangleShape SFMLShape;
+        public SFMLSquare2DComp():base(){
+            SFMLShape = new SFML.Graphics.RectangleShape();
+        }
+        public override void Draw()
+        {
+            SFMLShape.Size = new SFML.System.Vector2f(width, height);
+            SFMLShape.FillColor = getColor().convert();
+            if (gameObject.GetTransform() != null)
+                SFMLShape.Position = gameObject.GetTransform().Position.convert();
+            WindowManager.getInstance().getRenderWin().Draw(SFMLShape);
+        }
+
+    }
+}
